e33cf7ee6027bbddf449b14c470cdd25c9d7d599
Ronielle Moreira <ronielle.souza63@aluno.ifce.edu.br>
2024-11-26 12:07:46 -0300

    first commit

---
+++ src/models/postsModels.js
@@ -0,0 +1,25 @@
+import { ObjectId } from "mongodb";
+import conectarAoBanco from "../config/dbconfig.js";
+
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
+
+export async function getTodosPosts(params) {
+    const db = conexao.db("nodeimer")
+    const colecao = db.collection("posts")
+    return colecao.find().toArray()
+}
+
+export async function criarPost(novoPost) {
+    const db = conexao.db("nodeimer")
+    const colecao = db.collection("posts")
+    return colecao.insertOne(novoPost)
+}
+
+export async function atualizarPost(id, novoPost) {
+    const db = conexao.db("nodeimer")
+    const colecao = db.collection("posts")
+    const objID = ObjectId.createFromHexString(id)
+    return colecao.updateOne({_id: new ObjectId(objID)}, {$set:novoPost})
+}
+
+
